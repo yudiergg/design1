6630585ec3af13e94d9e8d5325e61e3d556474e2
yudiergg <gkscks1248@gmail.com>
2025-01-02 17:58:10 +0900

    250102-금호 건설
    오늘 집에 가서 gnb 쪽 확인해보기

--- Kumho/js/common.js
+++ Kumho/js/common.js
@@ -1,1 +1,40 @@
 /* header 와 footer 공통요소에 들어가는 스크립트 저장 모든 페이지에서 공통으로 작용하는 스크립트 */
+$(document).ready(function(){
+    let scrolling = 0
+    let prev_scroll
+
+
+    /* header에 fixed랑 scroll_dwon 추가 시작 */
+    /* 스크롤이 1이라도 되면 fixed 클래스 추가
+        하단을 스크롤 되면 scroll_down 클래스 추가
+        위로 스크롤 되면 scroll_down 클래스 삭제
+        맨위로 갔을때 fixed, scroll_down 모두 삭제
+        스크롤 방향 판단
+        이전 스크롤값을 저장해두고 현재 스크롤 값을 빼면
+    */
+    function scroll_chk(){
+        prev_scroll = scrolling
+        scrolling = $(window).scrollTop()
+        if(scrolling > 0){
+            $('header').addClass('fixed')
+            if((prev_scroll - scrolling < 0)){
+                $('header').addClass('scroll_down')
+                console.log('내려감')  
+            }
+            else{
+                $('header').removeClass('scroll_down')
+                console.log('올라감')
+            }
+        }else{
+            $('header').removeClass('fixed')
+            $('header').removeClass('scroll_down')
+        }
+
+        // console.log(prev_scroll, scrolling)
+    }
+    scroll_chk() /* 함수 실행해야나오며 문서가 로딩되었을때 한번임 */
+    $(window).scroll(function(){
+        scroll_chk() // 스크롤할때마다 실행
+    })
+});
+
--- Kumho/js/main.js
+++ Kumho/js/main.js
@@ -2,22 +2,31 @@ $(document).ready(function(){
     const visual_swiper = new Swiper('.visual .swiper', { 
 
     autoplay: {  
-        delay: 2500,
+        delay: 5000,
         disableOnInteraction: true,
     },
 
     loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
     pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-        el: '.swiper-pagination', /* 해당 요소의 class명 */
+        el: '.visual .ctrl_wrap .paging', /* 해당 요소의 class명 */
         clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
         type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-        renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-            return '<span class="' + className + '">' + (index + 1) + "</span>";
-        },
+        // renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+        //     return '<span class="' + className + '"> 안녕' + (index + 1) + "</span>";
+        // },
     },
 
     });
-    visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-    visual_swiper.autoplay.start();  /* 재생 기능 */
+   
+    $('.visual .ctrl_wrap .stop').on('click',function(){
+        visual_swiper.autoplay.stop();
+        $(this).hide()
+        $('.visual .ctrl_wrap .play').show()
+    });
+    $('.visual .ctrl_wrap .play').on('click',function(){
+        visual_swiper.autoplay.start();
+        $(this).hide()
+        $('.visual .ctrl_wrap .stop').show()
+    });
 });
