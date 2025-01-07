2d821cc09294b1cb2167b25efe34575e14384cea
yudiergg <gkscks1248@gmail.com>
2025-01-07 18:03:26 +0900

    ..
    ..

--- Kumho/js/main.js
+++ Kumho/js/main.js
@@ -47,4 +47,10 @@ $(document).ready(function(){
             dragSize: 140,
           },
     });
+    
+    AOS.init({
+        offset: 150, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
+        duration: 500, // 애니메이션 효과가 작동되는 시간
+        easing: 'ease', // 가속도
+        });
 });
