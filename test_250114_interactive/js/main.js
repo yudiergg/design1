e52b9131e23b0d879cfe9a341ec23a4d3cf632ba
yudiergg <gkscks1248@gmail.com>
2025-01-15 13:12:59 +0900

    250115 - 스크롤내릴때 인터레티브
    ..

--- test_250114_interactive/js/main.js
+++ test_250114_interactive/js/main.js
@@ -3,14 +3,12 @@ $(document).ready(function(){
     /* 공통요소 */    
     let scrolling = $(window).scrollTop() // 현재 스크롤 된 값
     let window_h = $(window).height() //브라우저 높이
-
     $(window).scroll(function(){
         scrolling = $(window).scrollTop()
     })
     $(window).resize(function(){
         window_h = $(window).height()
     })
-    /* 공통 요소 종료 */
 
     /* 해당 변수값의 선택자들만 조정하면됨 */
     let obj_wrap = $('.txt_slide') //애니메이션이 들어갈 글자 전체를 감싸는 요소
@@ -113,6 +111,97 @@ $(document).ready(function(){
     })
     $(window).resize(function(){ // 브라우저가 리사이즈가 될때
         photo_resize()
-    })    
+    })
+
+    /* 스크롤되면 배경이 검은색으로 됨
+    스크롤 시작점 일때는 bg_change 클래스 black 이 추가
+    스크롤 끝나는 높이일때 bg_change에 클래스 black이 사라짐
+    스크롤 엔드에서 시작점으로 다시 올라갈경우 다시 bg_chage에 black이 추가 */
+    
+    let bg_name = $('.bg_change')
+    let bg_class = 'black'
+    let bg_start // 배경색 바꿔주는거
+
+    function bg_chage(){
+        bg_start = bg_name.offset().top - (window_h * 0)
+        // console.log('스크롤',scrolling, '.bg_chage top값', bg_start)
+        if(scrolling < bg_start){
+            bg_name.removeClass(bg_class)
+        }else{
+            bg_name.addClass(bg_class)
+        }
+    }
+    bg_chage()
+
+    $(window).scroll(function(){
+        bg_chage()
+    })
+    $(window).resize(function(){
+        bg_chage()
+    })
+
+    /* 동영상 컨텐츠 시작점은 default
+    1. .bg_change .video_fit .movie_
+        - 브라우저 상단에 도달하기이전 data-status="before"
+        - 도달해서 스크롤 되는 중 data-status="fixed"
+        - 이미지 스크롤 되어서 화면 밖으로 사라지는 경우 data-status="after"
+        스크롤되는 도중 이벤트가 실행될때 영역이 천천히 늘어나거나 천천히 줄어들거임
+        스크롤 1000일때 고정 시작 
+        스크롤 2000일때 고정 종료 ---- 스크롤 1000동안 사이즈가 리사이즈됨
+        리사이즈 시작 값은 80 
+        리사이즈 종료 값은 100
+        예를 들어 1500일때 현재 넓이가 얼마
+        vf+area_gap = 2000(vf_end) - 1000(vf_satrt)
+        1500(scrolling) - 1000(vf_satrt) = 500 / 1000(vf_area_gap) =  0.5
+        100(vf_resize_end) - 80(vf_resize_start) = 20 * 0.5 = 10 */
+
+    let vf_area_name = $('.bg_change .video_fit .movie_h')
+    let vf_start // 영역 고정 시작점
+    let vf_end // 영역 고정 종료점
+    let vf_resize_name = $('.bg_change .video_fit .movie_h .movie .movie_inner')
+    let vf_resize_start = 80
+    let vf_resize_end = 100
+    let vf_resize_w // 리사이즈 될때 계산한 넓이값
+    let vf_scroll_per // 스크롤된 값의 퍼센트
+    let vf_area_gap // 리사이즈를 계산해야할 스크롤 구간값
+
+    function video_fixed(){
+        vf_start = vf_area_name.offset().top
+        vf_end = vf_area_name.offset().top + vf_area_name.height() - window_h
+        vf_area_gap = vf_end - vf_start
+
+        if(scrolling < vf_start){
+            console.log('아직 아니다')
+            vf_area_name.attr('data-status', 'before') //기존값을 지우고 내가 준값으로 교체
+            vf_resize_w = vf_resize_start
+        }else if(scrolling < vf_end){
+            console.log('고정')
+            vf_area_name.attr('data-status', 'fixed')
+            vf_scroll_per = (scrolling - vf_start) / vf_area_gap
+            vf_resize_w = ((vf_resize_end - vf_resize_start) * vf_scroll_per) + vf_resize_start
+            vf_resize_w = vf_resize_w * 0.8
+            if(vf_resize_w < vf_resize_end){ //end 값 이상 못늘어나게 하는거
+                vf_resize_w = vf_resize_end
+            }
+        }else{
+            console.log('끝')
+            vf_area_name.attr('data-status', 'after')
+            vf_resize_w = vf_resize_end
+        }
+        vf_resize_name.width(vf_resize_w + '%')
+        vf_resize_name.height(vf_resize_w + '%')
+    }
+
+
+    video_fixed() //함수 괄호는 브라우저 로딩될때 한번임
+
+    $(window).scroll(function(){
+        video_fixed() 
+    })
+    $(window).resize(function(){
+        video_fixed() 
+    })
+
+    
 
 })
